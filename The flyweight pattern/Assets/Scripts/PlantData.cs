e64f00a1ef871449b41b882063050a9ed5bdb630
Pahutich <dima.gamedev@pm.me>
2023-08-12 14:40:23 +0200

    Scriptable objects part 1

---
+++ The flyweight pattern/Assets/Scripts/PlantData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "plantData", menuName = "Plant Data", order = 51)]
+public class PlantData : ScriptableObject
+{
+    public enum THREAT {NONE, LOW, MODERATE, HIGH}
+    [SerializeField] private string plantName;
+    [SerializeField] private THREAT plantThreat;
+    [SerializeField] private Texture icon;
+}
