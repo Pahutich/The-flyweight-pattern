c3657dc71115ad02dc0a46af36c7fb10ef164f14
Pahutich <dima.gamedev@pm.me>
2023-08-12 14:57:23 +0200

    Scriptable objects part 2

--- The flyweight pattern/Assets/Scripts/Plant.cs
+++ The flyweight pattern/Assets/Scripts/Plant.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plant : MonoBehaviour
 {
     [SerializeField] private PlantData info;
+    SetPlantInfo spi;
+
+    private void Start() {
+        spi = GameObject.FindGameObjectWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.plantName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player") && info.Threat == PlantData.THREAT.HIGH)
+        {
+            PlayerController.dead = true;
+        }
+    }
 }
--- The flyweight pattern/Assets/Scripts/PlantData.cs
+++ The flyweight pattern/Assets/Scripts/PlantData.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "plantData", menuName = "Plant Data", order = 51)]
 public class PlantData : ScriptableObject
 {
-    public enum THREAT {NONE, LOW, MODERATE, HIGH}
-    [SerializeField] private string plantName;
-    [SerializeField] private THREAT plantThreat;
-    [SerializeField] private Texture icon;
+  public enum THREAT { NONE, LOW, MODERATE, HIGH }
+  [SerializeField] private string plantName;
+  [SerializeField] private THREAT plantThreat;
+  [SerializeField] private Texture icon;
+
+  public string Name { get { return plantName; } }
+  public THREAT Threat { get { return plantThreat; } }
+  public Texture Icon { get { return icon; } }
 }
